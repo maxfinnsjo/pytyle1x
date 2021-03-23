a6c59b4ab82767bacc5945da89fa8e1b7d369d86
programical <32033530+programical@users.noreply.github.com>
2021-03-23 11:01:30 +0100

    release 0.8.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools, sys
 
 setuptools.setup(
     name = 'pytyle1x',
-    version = '0.8.1',
+    version = '0.8.2',
     author = 'programical',
     description = 'A tiling manager for EWMH compliant window managers',
     long_description = open('README.md', 'r').read(),
